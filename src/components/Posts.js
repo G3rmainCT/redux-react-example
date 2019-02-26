b441e892f905b716f2fabb35e73f03568499a110
Germain C <germain.carino@outlook.com>
2019-02-26 09:59:30 -0600

    Add initial components and call to /posts API

---
+++ src/components/Posts.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+class Posts extends Component {  
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentWillMount() {
+    fetch(POST_URL)
+    .then(response => response.json())
+    .then((posts) => this.setState({
+      posts: posts
+    }));
+    
+  }
+  render() {
+    const postItems = this.state.posts.map((postItem) => (
+      <div key={postItem.id}>
+        <h3> {postItem.title}</h3>
+        <p> {postItem.body}</p>
+      </div>
+    ));
+
+    return (
+      <div style={{margin: '30px'}}>
+        <h1>Posts</h1>
+        {postItems}
+      </div>
+    );
+  }
+}
+
+export default Posts;
